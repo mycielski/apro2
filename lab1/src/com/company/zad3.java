39b1254bb3f4caecaf1fb80b4c63c664491711c6
tmyciels <01149913@pw.edu.pl>
2021-03-25 13:35:58 +0100

    z3

--- lab1/src/com/company/zad3.java
+++ lab1/src/com/company/zad3.java
@@ -2,48 +2,68 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Stack;
 
 public class zad3 {
 
+    public static void main(String[] args) {
+        Numbers numbers = new Numbers(654);
+        System.out.println(numbers.getMedian());
+        numbers.insertNumber(656);
+        System.out.println(numbers.getMedian());
+        numbers.insertNumber(958);
+        System.out.println(numbers.getMedian());
+        numbers.insertNumber(699);
+        System.out.println(numbers.getMedian());
+        numbers.removeMedianNumber();
+        System.out.println(numbers.getMedian());
+    }
 }
 
 class Numbers {
 
     private int median;
     private boolean even = true;
-    private ArrayList<Integer> maxHeap = new ArrayList<Integer>();
-    private ArrayList<Integer> minHeap = new ArrayList<Integer>();
+    private final ArrayList<Integer> maxHeap = new ArrayList<Integer>();
+    private final ArrayList<Integer> minHeap = new ArrayList<Integer>();
 
     public Numbers(int firstNumber) {
         median = firstNumber;
-        even = !even;
+        even = false;
     }
 
     public double getMedian() {
         if (!even) return median;
-        return (Collections.min(minHeap)/2 + Collections.max(maxHeap)/2);
+        return (Collections.min(minHeap) / 2 + Collections.max(maxHeap) / 2);
     }
 
-    public void insertNumber(int number){
-        if (even){
-            if (number < Collections.min(minHeap) && number > Collections.max(maxHeap)){
+    public void insertNumber(int number) {
+        if (even) {
+            if (number < Collections.min(minHeap) && number > Collections.max(maxHeap)) {
                 median = number;
-                even = !even;
-            } else if (number <= Collections.max(maxHeap)){
+            } else if (number <= Collections.max(maxHeap)) {
                 median = Collections.max(maxHeap);
                 maxHeap.remove(Collections.max(maxHeap));
                 maxHeap.add(number);
-            } else if (number >= Collections.min(minHeap)){
+            } else if (number >= Collections.min(minHeap)) {
                 median = Collections.min(minHeap);
                 minHeap.remove(Collections.min(minHeap));
                 minHeap.add(number);
             }
-
         } else {
-            maxHeap.add(Math.min(number,median));
-            minHeap.add(Math.max(number,median));
-            even = !even;
+            maxHeap.add(Math.min(number, median));
+            minHeap.add(Math.max(number, median));
         }
+        even = !even;
+    }
+
+    public void removeMedianNumber() {
+        if (even) {
+            minHeap.remove(Collections.min(minHeap));
+            maxHeap.remove(Collections.max(maxHeap));
+            median = Collections.min(minHeap) / 2 + Collections.max(maxHeap) / 2;
+        } else {
+            median = Collections.min(minHeap) / 2 + Collections.max(maxHeap) / 2;
+        }
+        even = !even;
     }
 }
