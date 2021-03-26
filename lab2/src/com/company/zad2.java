f985edab568d99d063247d7e5cb1757799e85d89
tmyciels <01149913@pw.edu.pl>
2021-03-26 22:04:14 +0100

    z2 co ja mam tu dalej robić???

--- lab2/src/com/company/zad2.java
+++ lab2/src/com/company/zad2.java
@@ -1,18 +1,60 @@
 package com.company;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class zad2 {
+
+    public static void main(String[] args){
+        BinaryTree tree = new BinaryTree("First");
+        tree.addNode("Second");
+    }
 }
 
-class BinaryTree implements Iterable {
+class BinaryTree {
 
-    @Override
-    public Iterator iterator() {
-        return null;
+    private Node root;
+
+    public BinaryTree(Object first) {
+        this.root = new Node(first);
+    }
+
+    public void addNode(Object o){
+        Node newNode = new Node(o);
+
+    }
+
+    class TreeIterator implements Iterator{
+
+        @Override
+        public boolean hasNext() {
+            try {
+                return next() != null;
+            } catch (NoSuchElementException e){
+                return false;
+            }
+        }
+
+        @Override
+        public Node next() {
+            return null;
+        }
     }
 
     class Node {
+        private Node leftChild = null, rightChild = null;
+        private Object value;
 
+        public Node(Object value) {
+            this.value = value;
+        }
+
+        public void setLeftChild(Node leftChild) {
+            this.leftChild = leftChild;
+        }
+
+        public void setRightChild(Node rightChild) {
+            this.rightChild = rightChild;
+        }
     }
 }
