4556c03da21a636f81e365a613689461dc4ef444
tmyciels <01149913@pw.edu.pl>
2021-03-26 21:28:38 +0100

    z2 initial commit

--- lab2/src/com/company/zad2.java
+++ lab2/src/com/company/zad2.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import java.util.Iterator;
+
 public class zad2 {
+}
+
+class BinaryTree implements Iterable {
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    class Node {
+
+    }
 }
