e822f08f2916dec73e940a05b78ab33e35655989
tmyciels <01149913@pw.edu.pl>
2021-03-26 22:46:35 +0100

    z2 o co w tym chodzi wgl

--- lab2/src/com/company/zad2.java
+++ lab2/src/com/company/zad2.java
@@ -5,50 +5,68 @@ import java.util.NoSuchElementException;
 
 public class zad2 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BinaryTree tree = new BinaryTree("First");
         tree.addNode("Second");
     }
 }
 
-class BinaryTree implements Iterable{
+class BinaryTree implements Iterable {
 
-    private Node root;
+    private final Node root;
 
     public BinaryTree(Object first) {
         this.root = new Node(first);
     }
 
-    public void addNode(Object o){
+    public void addNode(Object o) {
         Node newNode = new Node(o);
+        TreeIterator treeIterator = new TreeIterator(root);
 
     }
 
     @Override
     public Iterator iterator() {
+        //TreeIterator treeIterator = new TreeIterator();
         return null;
     }
 
-    class TreeIterator implements Iterator{
+    class TreeIterator implements Iterator<Node> {
+
+        private Node current, parent;
+
+        public TreeIterator(Node root) {
+            current = root;
+            parent = root;
+        }
 
         @Override
         public boolean hasNext() {
             try {
                 return next() != null;
-            } catch (NoSuchElementException e){
+            } catch (NoSuchElementException e) {
                 return false;
             }
         }
 
         @Override
         public Node next() {
-            return null;
+
+            return current;
+        }
+
+        public Node last(){
+            while (current.getRightChild() != null){
+                current = current.getRightChild();
+                if (current.getLeftChild() == null) return current;
+            }
+            return current.getLeftChild();
         }
     }
 
     class Node {
         private Node leftChild = null, rightChild = null;
-        private Object value;
+        private final Object value;
 
         public Node(Object value) {
             this.value = value;
@@ -60,6 +78,18 @@ class BinaryTree implements Iterable{
 
         public void setRightChild(Node rightChild) {
             this.rightChild = rightChild;
+        }
+
+        public Node getLeftChild() {
+            return leftChild;
+        }
+
+        public Node getRightChild() {
+            return rightChild;
+        }
+
+        public Object getValue() {
+            return value;
         }
     }
 }
