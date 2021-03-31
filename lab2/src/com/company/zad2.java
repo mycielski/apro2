73ca5aedb82bf4fd0921d108ea3f1dafc198a185
tmyciels <01149913@pw.edu.pl>
2021-03-31 22:38:32 +0200

    z2 zwracanie wartości posortowanych

--- lab2/src/com/company/zad2.java
+++ lab2/src/com/company/zad2.java
@@ -1,20 +1,38 @@
 package com.company;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class zad2 {
 
     public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree(1);
-        tree.degenerateAddNode(2);
-        tree.degenerateAddNode(3);
-        tree.degenerateAddNode(4);
-        tree.degenerateAddNode(5);
-        tree.degenerateAddNode(6);
-        tree.degenerateAddNode(7);
-        tree.degenerateAddNode(8);
-        List<Integer> list = new ArrayList<>();
+        System.out.println("\n~~REGULAR BINARY TREE~~");
+        BinaryTree tree = new BinaryTree(5);
+        tree.addNode(2);
+        tree.addNode(3);
+        tree.addNode(4);
+        tree.addNode(1);
+        tree.addNode(6);
+        tree.addNode(7);
+        tree.addNode(8);
+        System.out.println("SORTED:");
+        for (Object value : tree.getSortedValues()) System.out.println(value);
+        System.out.println("NOT SORTED:");
         for (Object leaf : tree) System.out.println(leaf);
+
+        System.out.println("\n~~DEGENERATE BINARY TREE~~");
+        BinaryTree degenerateTree = new BinaryTree(8);
+        degenerateTree.degenerateAddNode(3);
+        degenerateTree.degenerateAddNode(2);
+        degenerateTree.degenerateAddNode(4);
+        degenerateTree.degenerateAddNode(5);
+        degenerateTree.degenerateAddNode(6);
+        degenerateTree.degenerateAddNode(7);
+        degenerateTree.degenerateAddNode(1);
+        System.out.println("SORTED:");
+        for (Object value : degenerateTree.getSortedValues()) System.out.println(value);
+        System.out.println("NOT SORTED:");
+        for (Object leaf : degenerateTree) System.out.println(leaf);
 
     }
 }
@@ -26,6 +44,13 @@ class BinaryTree implements Iterable {
 
     public BinaryTree(Object rootNodeValue) {
         this.root = new Node(rootNodeValue);
+    }
+
+    public LinkedList getSortedValues(){
+        LinkedList list = new LinkedList();
+        TreeIterator ti = new TreeIterator(root);
+        while (ti.hasNext()) list.add(ti.next());
+        return (LinkedList) list.stream().sorted(Comparator.comparing(Node::hashCode)).collect(Collectors.toCollection(LinkedList::new));
     }
 
     public void addNode(Object newNodeValue) {
@@ -136,108 +161,10 @@ class BinaryTree implements Iterable {
             if (hasRightChild()) sb.append(", ").append("rightChildValue=").append(getRightChild().getValue());
             return sb.append('}').toString();
         }
-    }
-}
-
-/*
-class BinaryTree<Node> implements Iterable<BinaryTree.Node> {
-
-    private final Node root;
-    private Node current;
-    private Queue<Node> q;
-
-    public BinaryTree(Object first) {
-        root = new Node(first);
-    }
-
-    public void addNode(Object o) {
-        Node newNode = new Node(o);
-        Iterator<Node> iterator = iterator();
-        while(iterator.hasNext()) iterator.next();
-        iterator.next().setLeftChild(newNode);
-    }
-
-    @Override
-    public Iterator<Node> iterator() {
-        q.add(root);
-        Iterator<Node> it = new Iterator<Node>() {
-
-            @Override
-            public boolean hasNext() {
-                return q.peek() != null;
-            }
-
-            @Override
-            public Node next() {
-                if(q.peek().getLeftChild()!=null) q.add(q.peek().getLeftChild());
-                if(q.peek().getRightChild()!=null) q.add(q.peek().getRightChild());
-                return q.remove();
-            }
-        };
-        return it;
-    }
-
-    /*
-    class TreeIterator implements Iterator<Node> {
-
-        private Node current;
-        private Queue<Node> q;
-
-        public TreeIterator(Node root) {
-            current = root;
-            q.add(root);
-            if(root.getLeftChild()!= null) q.add(root.getLeftChild());
-            if(root.getRightChild() != null) q.add(root.getRightChild());
-        }
 
         @Override
-        public boolean hasNext() {
-            return q.peek() != null;
-        }
-
-        @Override
-        public Node next() {
-            current = q.remove();
-            if(current.getLeftChild()!= null) q.add(current.getLeftChild());
-            if(current.getRightChild()!=null)q.add(current.getRightChild());
-            return current;
-        }
-
-    }
-
-
-
-    class Node {
-        private Node leftChild = null, rightChild = null;
-        private Object value;
-
-        public Node(Object value) {
-            this.value = value;
-        }
-
-        public void setLeftChild(Node leftChild) {
-            this.leftChild = leftChild;
-        }
-
-        public void setRightChild(Node rightChild) {
-            this.rightChild = rightChild;
-        }
-
-        public Node getLeftChild() {
-            return leftChild;
-        }
-
-        public Node getRightChild() {
-            return rightChild;
-        }
-
-        public Object getValue() {
-            return value;
-        }
-
-        public void setValue(Object value) {
-            this.value = value;
+        public int hashCode() {
+            return value.hashCode();
         }
     }
 }
-*/
