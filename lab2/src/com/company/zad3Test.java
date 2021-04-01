b05144ffe521f3d3f07f7738924f26009122e447
tmyciels <01149913@pw.edu.pl>
2021-04-01 11:46:35 +0200

    zad 3 test zrobione

--- lab2/src/com/company/zad3Test.java
+++ lab2/src/com/company/zad3Test.java
@@ -6,15 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class zad3Test {
 
-    private static BinaryTree tree = new BinaryTree(1);
+    private static BinaryTree completeTree = new BinaryTree(1);
+    private static BinaryTree incompleteTree = new BinaryTree(1);
 
+    @BeforeAll
+    static void beforeAllTest(){
+        for (int i = 2; i < 10; i++) {
+            completeTree.addNode(i);
+            incompleteTree.degenerateAddNode(i);
+        }
+    }
     @BeforeEach
     void beforeEachTest(){
-        tree.addNode(Math.random());
+        completeTree.addNode(Math.random());
+        incompleteTree.addNode(Math.random());
+
     }
 
-    @RepeatedTest(50)
+    @RepeatedTest(20)
     void isComplete() {
-        assertTrue(zad3.isComplete(zad3Test.tree));
+        assertTrue(zad3.isComplete(zad3Test.completeTree));
+        assertFalse(zad3.isComplete(zad3Test.incompleteTree));
     }
 }
