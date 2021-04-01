9e8fc8a64a55a23721f75447f2c5ed848bdc6d06
tmyciels <01149913@pw.edu.pl>
2021-04-01 11:42:02 +0200

    zad 3 test

--- lab2/src/com/company/zad3Test.java
+++ lab2/src/com/company/zad3Test.java
@@ -1,12 +1,20 @@
 package com.company;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class zad3Test {
 
-    @Test
+    private static BinaryTree tree = new BinaryTree(1);
+
+    @BeforeEach
+    void beforeEachTest(){
+        tree.addNode(Math.random());
+    }
+
+    @RepeatedTest(50)
     void isComplete() {
+        assertTrue(zad3.isComplete(zad3Test.tree));
     }
 }
