bc23e21938937239e7ceb8fe6f8461734304aadf
tmyciels <01149913@pw.edu.pl>
2021-04-01 11:20:00 +0200

    zad 3

--- lab2/src/com/company/zad3.java
+++ lab2/src/com/company/zad3.java
@@ -1,4 +1,43 @@
 package com.company;
 
+import com.company.BinaryTree.Node;
+
+import java.util.LinkedList;
+
 public class zad3 {
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree(1);
+        tree.addNode(2);
+        tree.addNode(3);
+        tree.addNode(4);
+        tree.addNode(5);
+        tree.addNode(6);
+        tree.addNode(7);
+        tree.addNode(8);
+        tree.degenerateAddNode(9);
+        tree.degenerateAddNode(10);
+        tree.degenerateAddNode(11);
+        System.out.println(isComplete(tree));
+    }
+
+    public static boolean isComplete(BinaryTree tree) {
+        Node current;
+        LinkedList<Node> list = new LinkedList<>();
+        boolean potentiallyIncomplete = false;
+        for (Object leaf : tree) {
+            current = (Node) leaf;
+            if (!potentiallyIncomplete && current.hasLeftChild()) {
+                list.add(current.getLeftChild());
+                if (current.hasRightChild()) {
+                    list.add(current.getRightChild());
+                } else {
+                    potentiallyIncomplete = true;
+                }
+            } else {
+                if (current.hasRightChild() || current.hasLeftChild()) return false;
+            }
+        }
+        return true;
+    }
 }
