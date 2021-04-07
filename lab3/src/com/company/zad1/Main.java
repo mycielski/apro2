50c7235eb920f30ddbbd57898c36587797dfcb0d
tmyciels <01149913@pw.edu.pl>
2021-04-08 00:38:17 +0200

    z1 dobre złego początki

--- lab3/src/com/company/zad1/Main.java
+++ lab3/src/com/company/zad1/Main.java
@@ -1,8 +1,19 @@
 package com.company.zad1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Podaj słowo:");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
     }
+
+    public static int countSubstringAnagrams(String word){
+        int count = 0;
+
+        return count;
+    }
+
 }
