55da4feed8a31c0a7926c001a523858a32cbe443
tmyciels <01149913@pw.edu.pl>
2021-04-08 00:15:53 +0200

    z2 - znajduje sciezke, działa

--- lab3/src/com/company/zad2/Graph.java
+++ lab3/src/com/company/zad2/Graph.java
@@ -7,13 +7,14 @@ import java.util.Queue;
 
 public class Graph {
 
-    private final int vertices,edges;
+    private final int vertices, edges;
     private final ArrayList<ArrayList<Integer>> adjacencyList;
 
     /**
      * Initializes the graph based on its number of vertices and edges.
+     *
      * @param vertices total number of vertices in this graph
-     * @param edges total number of edges in this graph
+     * @param edges    total number of edges in this graph
      */
     public Graph(int vertices, int edges, ArrayList<ArrayList<Integer>> adjacencyList) {
         this.vertices = vertices;
@@ -22,45 +23,76 @@ public class Graph {
     }
 
     /**
-     *  prints out the path between two vertices
+     * prints out the path between two vertices
+     *
      * @param destination index of destination vertex
-     * @param source index of source vertex
+     * @param source      index of source vertex
      */
-    public void printShortestPathBetweenTwoVertices(int destination, int source){
+    public void printShortestPathBetweenTwoVertices(int destination, int source) {
+        for (Integer v : adjacencyList.get(source)) {
+            if (v.equals(destination)) {
+                System.out.println(source + " " + destination);
+                return;
+            }
+        }
         int[] predecessor = new int[vertices];
         int[] distance = new int[vertices];
 
-        if (!BFS(destination,source,predecessor,distance)) {
+        if (!BFS(destination, source, predecessor, distance)) {
             System.out.println("Podane wierzchołki nie są połączone.");
             return;
         }
         LinkedList<Integer> path = new LinkedList<Integer>();
         int crawl = source;
         path.add(crawl);
-        while(predecessor[crawl] != -1){
+        while (predecessor[crawl] != -1) {
             path.add(predecessor[crawl]);
             crawl = predecessor[crawl];
         }
 
         StringBuilder stringBuilder = new StringBuilder();
-        for (Integer stop : path){
+        for (Integer stop : path) {
             stringBuilder.append(stop).append(" ");
         }
         System.out.println(stringBuilder.toString());
 
     }
 
-    public int countDisjointedSubgraphs(){
+    public void printPathBetweenVertices(int source, int destination) {
+        boolean[] visited = new boolean[vertices];
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        if (DFS(source, destination, visited,path) == -1 ) System.out.println("Wierzchołki nie są połączone.");
+    }
+
+    public int DFS(int source, int destination, boolean[] visited, ArrayList<Integer> path) {
+        if (!visited[source]) {
+            visited[source]= true;
+            path.add(source);
+            for (Integer vertex : adjacencyList.get(source)) {
+                if (vertex == destination) {
+                    path.add(destination);
+                    for(Integer step : path) System.out.println(step);
+                    return source;
+                }
+            }
+            for (Integer vertex : adjacencyList.get(source)) {
+                return DFS(vertex, destination, visited, path);
+            }
+        }
+        return -1;
+    }
+
+    public int countDisjointedSubgraphs() {
         boolean[] visited = new boolean[vertices];
         int count = 0;
         Queue<Integer> queue = new LinkedList<Integer>();
         for (int i = 0; i < vertices; i++) {
-            if (!visited[i]){
+            if (!visited[i]) {
                 queue.add(i);
-                while(!queue.isEmpty()){
+                while (!queue.isEmpty()) {
                     int current = queue.remove();
                     visited[current] = true;
-                    for(Integer vertex : adjacencyList.get(current)) {
+                    for (Integer vertex : adjacencyList.get(current)) {
                         if (!visited[vertex]) queue.add(vertex);
                     }
                 }
@@ -70,11 +102,11 @@ public class Graph {
         return count;
     }
 
-    private boolean BFS(int source, int destination, int[] predecessor, int[] distance){
+    private boolean BFS(int source, int destination, int[] predecessor, int[] distance) {
         LinkedList<Integer> queue = new LinkedList<Integer>();
         boolean[] visited = new boolean[vertices];
 
-        for (int i = 0; i < vertices; i++){
+        for (int i = 0; i < vertices; i++) {
             visited[i] = false;
             distance[i] = Integer.MAX_VALUE;
             predecessor[i] = -1;
@@ -84,17 +116,17 @@ public class Graph {
         distance[source] = 0;
         queue.add(source);
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int u = queue.remove();
-            for (int i = 0; i < adjacencyList.get(u).size(); i++){
-                if (!visited[adjacencyList.get(u).get(i)]){
+            for (int i = 0; i < adjacencyList.get(u).size(); i++) {
+                if (!visited[adjacencyList.get(u).get(i)]) {
                     visited[adjacencyList.get(u).get(i)] = true;
                     distance[adjacencyList.get(u).get(i)] = distance[u] + 1;
                     predecessor[adjacencyList.get(u).get(i)] = u;
                     queue.add(adjacencyList.get(u).get(i));
 
                     // break upon finding destination
-                    if(adjacencyList.get(u).get(i).equals(destination)) return true;
+                    if (adjacencyList.get(u).get(i).equals(destination)) return true;
                 }
             }
         }
--- lab3/src/com/company/zad2/Main.java
+++ lab3/src/com/company/zad2/Main.java
@@ -4,14 +4,41 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Graph graph = generateGraphFromFile("moj_graf.txt");
-        //System.out.println(graph.countDisjointedSubgraphs());
-        graph.printShortestPathBetweenTwoVertices(0,3);
+        int choice = 1;
+        System.out.println("Podaj ścieżkę do pliku z grafem");
+        Scanner scanner = new Scanner(System.in);
+        String filepath = scanner.nextLine();
+        Graph graph = generateGraphFromFile(filepath);
+        while (choice != 0){
+            System.out.println("Wybierz co chcesz zrobić:");
+            System.out.println("1 - liczenie spójnych składowych grafu");
+            System.out.println("2 - szukanie ścieżki między wierzchołkami grafu");
+            System.out.println("0 - wyjście z programu");
+            choice = scanner.nextInt();
+            switch(choice) {
+                case 1:
+                    System.out.println(graph.countDisjointedSubgraphs());
+                    break;
+                case 2:
+                    System.out.println("Podaj indeksy wierzchołków między którymi chcesz wyznaczyć ścieżkę.");
+                    int v1 = scanner.nextInt();
+                    int v2 = scanner.nextInt();
+                    graph.printPathBetweenVertices(v1,v2);
+                    //graph.printShortestPathBetweenTwoVertices(v2,v1);
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Nie rozumiem. Spróbuj ponownie.");
+            }
+
+        }
     }
 
     /**
@@ -36,7 +63,6 @@ public class Main {
             line = reader.readLine();
             edges = Integer.parseInt(line);
             line = reader.readLine();
-            System.out.println("v" + vertices + "e" + edges);
             while (line != null) {
                 // if first char of line is a # ignores the line
                 if (line.charAt(0) == '#') {
@@ -45,17 +71,12 @@ public class Main {
                     // splits the line at space character and parses the parts of the line to integer, then updates
                     // the adjacency list
                     String[] parts = line.split(" ");
-                    for (int i = 0; i < parts.length; i++) System.out.print(parts[i]);
                     for (int i = 1; i < parts.length; i++){
                         adjacencyList.get(Integer.parseInt(parts[0])).add(Integer.parseInt(parts[i]));
                         adjacencyList.get(Integer.parseInt(parts[i-1])).add(Integer.parseInt(parts[0]));
                     }
                 }
-                System.out.println();
                 line = reader.readLine();
-            }
-            for (ArrayList lista : adjacencyList){
-                //System.out.println(lista);
             }
             return new Graph(vertices,edges,adjacencyList);
         } catch (IOException e) {
