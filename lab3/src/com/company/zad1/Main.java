d66a344ab9b9d355b1fe8b4dbfa7dc535533ba8e
tmyciels <01149913@pw.edu.pl>
2021-04-08 01:08:23 +0200

    z1 pracuje nad tym

--- lab3/src/com/company/zad1/Main.java
+++ lab3/src/com/company/zad1/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Podaj słowo:");
         Scanner scanner = new Scanner(System.in);
         String word = scanner.nextLine();
+        System.out.println("Odnaleziono " + countSubstringAnagrams(word) + " par substringów będących anagramami.");
     }
 
     public static int countSubstringAnagrams(String word){
