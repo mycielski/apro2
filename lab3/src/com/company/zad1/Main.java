b4fe9b821eb1cf1ef219f0b90f9339cf00bda24b
tmyciels <01149913@pw.edu.pl>
2021-04-08 10:44:47 +0200

    z1 pracuje nad tym

--- lab3/src/com/company/zad1/Main.java
+++ lab3/src/com/company/zad1/Main.java
@@ -1,6 +1,7 @@
 package com.company.zad1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -13,10 +14,22 @@ public class Main {
     }
 
     public static int countSubstringAnagrams(String word){
+        if (word.length() < 2) return -1;
         int count = 0;
-        for (int i = 0; i < word.length()-1; i++) {
+        for (int i = 1; i < word.length(); i++) {
             ArrayList<String> list = new ArrayList<String>();
-
+            ArrayList<String> listOfSortedString = new ArrayList<String>();
+            for(int j = 0; j < word.length()-i+1; j++){
+                // getting a substring and adding it to the list of substrings
+                String substring = word.substring(j,j+i);
+                list.add(substring);
+                // sorting the substring alphabetically and adding it to the list of sorted substrings
+                char[] substringAsArray = substring.toCharArray();
+                Arrays.sort(substringAsArray);
+                String substringSorted = new String(substringAsArray);
+                listOfSortedString.add(substringSorted);
+                System.out.println(substring +" " + substringSorted);
+            }
         }
         return count;
     }
