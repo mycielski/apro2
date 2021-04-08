c2c3a80bcf748a38f62a70831e82c8e223677fce
tmyciels <01149913@pw.edu.pl>
2021-04-08 11:17:14 +0200

    z1 code prettify and comments

--- lab3/src/com/company/zad1/Main.java
+++ lab3/src/com/company/zad1/Main.java
@@ -2,6 +2,7 @@ package com.company.zad1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -13,15 +14,16 @@ public class Main {
         System.out.println("Odnaleziono " + countSubstringAnagrams(word) + " par substringów będących anagramami.");
     }
 
-    public static int countSubstringAnagrams(String word){
+    public static int countSubstringAnagrams(String word) {
         if (word.length() < 2) return -1;
+        word = word.toLowerCase(Locale.ROOT);
         int count = 0;
         for (int i = 1; i < word.length(); i++) {
             ArrayList<String> list = new ArrayList<String>();
             ArrayList<String> listOfSortedString = new ArrayList<String>();
-            for(int j = 0; j < word.length()-i+1; j++){
+            for (int j = 0; j < word.length() - i + 1; j++) {
                 // getting a substring and adding it to the list of substrings
-                String substring = word.substring(j,j+i);
+                String substring = word.substring(j, j + i);
                 list.add(substring);
                 // sorting the substring alphabetically and adding it to the list of sorted substrings
                 char[] substringAsArray = substring.toCharArray();
@@ -31,10 +33,10 @@ public class Main {
             }
             //comparing the sorted strings and printing their versions before sorting if they are equal
             for (int k = 0; k < list.size(); k++) {
-                for (int l = k+1; l < list.size(); l++) {
-                    if(listOfSortedString.get(k).equals(listOfSortedString.get(l))){
+                for (int l = k + 1; l < list.size(); l++) {
+                    if (listOfSortedString.get(k).equals(listOfSortedString.get(l))) {
                         count++;
-                        System.out.println("[" + list.get(k) + " , " +list.get(l) + "]");
+                        System.out.println("[" + list.get(k) + " , " + list.get(l) + "]");
                     }
                 }
             }
