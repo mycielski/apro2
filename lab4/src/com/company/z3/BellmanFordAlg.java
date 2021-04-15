a310ada813184ac6ddf04759ae7e19470cc2ebb7
tmyciels <01149913@pw.edu.pl>
2021-04-15 19:44:13 +0200

    .

--- lab4/src/com/company/z3/BellmanFordAlg.java
+++ lab4/src/com/company/z3/BellmanFordAlg.java
@@ -19,7 +19,7 @@ public class BellmanFordAlg {
             distTo[v] = Double.POSITIVE_INFINITY;
         distTo[s] = 0.0;
 //Algorytm Bellmana-Forda
-        queue = new ListQueue<Integer>();
+        queue = new ListQueue<>();
         queue.enqueue(s);
         onQueue[s] = true;
         while (!queue.isEmpty()) {
@@ -30,22 +30,24 @@ public class BellmanFordAlg {
     }
 
     private void relax(DigraphWeighted G, int v) {
-        if (verbose) {
             System.out.println("Relaksacja krawędzi " + G);
             sleep();
-        }
         for (DirectEdge e : G.adj(v)) {
             int w = e.to();
-            System.out.println("Porównanie odległości do " + w + " i " + v);
+            if(verbose)System.out.println("Porównanie odległości do " + w + " i " + v);
             sleep();
             if (distTo[w] > distTo[v] + e.weight()) {
                 if (verbose) {
                     System.out.println("Odległość do " + w + " jest większa niż odległość do " + v);
                     sleep();
                 }
+                if(verbose) System.out.println("Aktualizowanie odległości do " + w);
+                sleep();
                 distTo[w] = distTo[v] + e.weight();
                 edgeTo[w] = e;
                 if (!onQueue[w]) {
+                    if (verbose) System.out.println("Dodawanie do kolejki " + w);
+                    sleep();
                     queue.enqueue(w);
                     onQueue[w] = true;
                 }
@@ -67,7 +69,7 @@ public class BellmanFordAlg {
         validateVertex(v);
         if (!hasPathTo(v)) return null;
         Stack<DirectEdge> path = new
-                Stack<DirectEdge>();
+                Stack<>();
         for (DirectEdge e = edgeTo[v]; e != null; e =
                 edgeTo[e.from()]) {
             path.push(e);
@@ -84,7 +86,7 @@ public class BellmanFordAlg {
     private void sleep() {
         if (verbose) {
             try {
-                TimeUnit.MILLISECONDS.sleep(555);
+                TimeUnit.MILLISECONDS.sleep(500);
             } catch (InterruptedException ignored) {
 
             }
