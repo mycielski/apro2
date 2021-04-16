fcefca84c96152a8acf98429cb028c58b8c07b11
tmyciels <01149913@pw.edu.pl>
2021-04-16 23:09:43 +0200

    Wprawka 2

--- zadanie2/src/com/company/CSVReader.java
+++ zadanie2/src/com/company/CSVReader.java
@@ -10,11 +10,48 @@ public class CSVReader {
     private final String[] headerRow;
     private final int columns;
     private final BufferedReader reader;
+    private final String filepath;
+    private int lines;
+    private final String[][] sheet;
 
     public CSVReader(String filepath) throws IOException {
+        this.filepath = filepath;
         reader = new BufferedReader(new FileReader(filepath));
         headerRow = reader.readLine().split(",");
         columns = headerRow.length;
+        countLines();
+        sheet = new String[lines][columns];
+        fillSheet();
+    }
+
+    public String[][] getSheet() {
+        return sheet;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    private void fillSheet() throws IOException {
+        for (int i = 0; i < columns; i++) {
+            sheet[0][i] = headerRow[i];
+        }
+        for (int i = 1; i < lines; i++) {
+            for (int j = 0; j < columns; j++) {
+                String[] row = reader.readLine().split(",");
+                sheet[i][j] = row[j];
+            }
+        }
+
+    }
+
+    private void countLines() {
+        try (BufferedReader lineReader = new BufferedReader(new FileReader(filepath))) {
+            lines = 0;
+            while (lineReader.readLine() != null) lines++;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String[] getHeaderRow() {
@@ -35,4 +72,7 @@ public class CSVReader {
         return builder.build();
     }
 
+    public int getLines() {
+        return lines;
+    }
 }
