76b51a2905cdd021ecb55781f30362ddbeb3b10d
tmyciels <01149913@pw.edu.pl>
2021-04-18 23:34:33 +0200

    lab5 initial commit

---
+++ lab5/src/com/company/z1/ConcatenatorPlusEquals.java
@@ -0,0 +1,10 @@
+package com.company.z1;
+
+public class ConcatenatorPlusEquals {
+
+    public static void main(String[] args) {
+        String concatenation;
+        concatenation = "String1" + "String2";
+        System.out.println(concatenation);
+    }
+}
---
+++ lab5/src/com/company/z1/ConcatenatorStringBuilder.java
@@ -0,0 +1,10 @@
+package com.company.z1;
+
+public class ConcatenatorStringBuilder {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("String1");
+        stringBuilder.append("String2");
+        System.out.println(stringBuilder);
+    }
+}
