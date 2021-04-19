21a01e84847be10ab305c8f83ff3e1038ad48afc
tmyciels <01149913@pw.edu.pl>
2021-04-19 07:50:51 +0200

    lab5

--- lab5/src/com/company/z1/ConcatenatorStringBuilder.java
+++ lab5/src/com/company/z1/ConcatenatorStringBuilder.java
@@ -2,7 +2,8 @@ package com.company.z1;
 
 public class ConcatenatorStringBuilder {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder stringBuilder;
+        stringBuilder = new StringBuilder();
         stringBuilder.append("String1");
         stringBuilder.append("String2");
         System.out.println(stringBuilder);
